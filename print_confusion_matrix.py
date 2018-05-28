6e6040b3b1454b65148e011d815c437151b598cd
Daniel Laguna <labellson@gmail.com>
2018-05-28 18:20:22 +0200

    Add row normalization option to print_confusion_matrix

--- print_confusion_matrix.py
+++ print_confusion_matrix.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-def print_confusion_matrix(confusion_matrix, class_names, figsize = (10,7), fontsize=14):
+def print_confusion_matrix(confusion_matrix, class_names, figsize = (10,7), fontsize=14, norm=False):
     """Prints a confusion matrix, as returned by sklearn.metrics.confusion_matrix, as a heatmap.
-    
+
     Arguments
     ---------
     confusion_matrix: numpy.ndarray
-        The numpy.ndarray object returned from a call to sklearn.metrics.confusion_matrix. 
+        The numpy.ndarray object returned from a call to sklearn.metrics.confusion_matrix.
         Similarly constructed ndarrays can also be used.
     class_names: list
         An ordered list of class names, in the order they index the given confusion matrix.
@@ -17,18 +18,28 @@ def print_confusion_matrix(confusion_matrix, class_names, figsize = (10,7), font
         the second determining the vertical size. Defaults to (10,7).
     fontsize: int
         Font size for axes labels. Defaults to 14.
-        
+    norm: bool
+    	Normalize confusion matrix rows
+
     Returns
     -------
     matplotlib.figure.Figure
         The resulting confusion matrix figure
     """
+    if norm:
+        confusion_matrix = confusion_matrix.copy().astype(np.float64)
+        total_samples = confusion_matrix.sum(axis=1)
+
+        for i in range(confusion_matrix.shape[0]):
+            confusion_matrix[i] = confusion_matrix[i] / total_samples[i]
+
     df_cm = pd.DataFrame(
-        confusion_matrix, index=class_names, columns=class_names, 
+        confusion_matrix, index=class_names, columns=class_names,
     )
     fig = plt.figure(figsize=figsize)
     try:
-        heatmap = sns.heatmap(df_cm, annot=True, fmt="d")
+        fmt = '.2f' if norm else 'd'
+        heatmap = sns.heatmap(df_cm, annot=True, fmt=fmt)
     except ValueError:
         raise ValueError("Confusion matrix values must be integers.")
     heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=fontsize)
